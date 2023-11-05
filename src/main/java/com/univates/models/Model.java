7a647856b610eae63bc3263f9dad7b9cfa31d539
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2023-11-05 15:25:09 -0300

    att

--- src/main/java/com/univates/models/Model.java
+++ src/main/java/com/univates/models/Model.java
@@ -390,7 +390,7 @@ public class Model<T>
                     Class.forName("org.postgresql.Driver");
                     String url      = "jdbc:postgresql://localhost:5432/postgres";
                     String user     = "kauan";
-                    String password = "Agente9961";
+                    String password = "senha";
                     Model.connection = DriverManager.getConnection(url, user, password);
                 } 
                 catch (Exception e) 
