66317ba7917988422786967dc81d9a4fc1ccf2a0
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2023-11-06 21:27:47 -0300

    Atualizando lista de a fazer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,8 @@ public class Main
 {
     /*
      * TODO:
-     *  1 - Trocar o LocalDateTime por Timestamp
-     *  2 - Funcao para poder ir para a tela de cadastro pela tela de login (feito)
-     *  3 - Clicar duas vezes em um item da tabela abre uma janela para editar ou excluir
-     *  4 - Fazer a tela de edicao
-     *  DONE 5 - Implementar o JDBC 
+     *  DOING 3 - Clicar duas vezes em um item da tabela abre uma janela para editar ou excluir
+     *  DOING 4 - Fazer a tela de edicao
      *  6 - Funcao de exportar para planilha
      *  7 - Funcao de importar de planilha    - Talvez nao de tempo
      *  8 - Funcao de baixar planilha modelo  - Talvez nao de tempo
