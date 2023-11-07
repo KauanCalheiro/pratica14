d5573502338ddd7adec1b15876c71683f5790a24
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2023-11-06 21:59:53 -0300

    alterando lista de a fazeres

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,12 @@ public class Main
 {
     /*
      * TODO:
-     *  DOING 3 - Clicar duas vezes em um item da tabela abre uma janela para editar ou excluir
-     *  DOING 4 - Fazer a tela de edicao
-     *  6 - Funcao de exportar para planilha
-     *  7 - Funcao de importar de planilha    - Talvez nao de tempo
-     *  8 - Funcao de baixar planilha modelo  - Talvez nao de tempo
+     *  DOING - Clicar duas vezes em um item da tabela abre uma janela para editar ou excluir
+     *  DOING - Fazer a tela de edicao
+     *  Alterar tamanho da celula da tabela 
+     *  Funcao de exportar para planilha
+     *  Funcao de importar de planilha    - Talvez nao de tempo
+     *  Funcao de baixar planilha modelo  - Talvez nao de tempo
      */
     public static void main(String[] args) throws Exception 
     {
